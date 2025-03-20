012623eb603016f5faea68db21821795d049e45a
Abdoul Karim <karimniang9@gmail.com>
2025-03-20 09:44:46 +0100

    Add First Challenge UdemyCourse JavaScript

---
+++ UdemyCourse/_01_Variables_DataType_And_More/Challenges/source.js
@@ -0,0 +1,25 @@
+const myString = 'developer';
+
+let finalString = myString[0].toUpperCase() + myString.slice(1,myString.length)
+
+console.log(finalString)
+
+
+// Second solution
+
+let finalString2 = myString.charAt(0).toUpperCase() + myString.slice(1,myString.length)
+
+console.log(finalString2)
+
+
+// Third solution
+
+let finalString3 = myString.charAt(0).toUpperCase() + myString.substring(1,myString.length)
+
+console.log(finalString3)
+
+
+// Fourth solution
+
+let finalString4 = myString[0].toUpperCase() + myString.split('').slice(1).join('')
+console.log(finalString4)
